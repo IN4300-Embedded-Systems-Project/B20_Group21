752b80834506e033904651e0c156284632377fa4
nimash3eshan <sudusinghene.20@itfac.mrt.ac.lk>
2025-03-01 22:05:23 +0530

    handled cors

--- Fast API/app.py
+++ Fast API/app.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 import httpx
+import uvicorn
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],    
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 ESP32_BASE_URL = "http://192.168.130.232:8080/servo"
 
@@ -20,3 +30,6 @@ async def close_gate():
         if response.status_code == 200:
             return {"message": "Gate closed successfully"}
         raise HTTPException(status_code=500, detail="Failed to close gate")
+    
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000, log_level="info")
